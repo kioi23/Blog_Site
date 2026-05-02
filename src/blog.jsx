cb0e926c243a57573c4f94f812f237e082511388
kioi23 <kioijgithinji23@gmail.com>
2026-05-02 13:17:42 +0300

    Connect components in App and pass data via props

---
+++ src/blog.jsx
@@ -0,0 +1,21 @@
+const blog = {
+  name: "My Blog",
+  image: "https://via.placeholder.com/150",
+  about: "Welcome to my blog!",
+  posts: [
+    {
+      id: 1,
+      title: "First Post",
+      date: "May 1, 2026",
+      preview: "This is my first post..."
+    },
+    {
+      id: 2,
+      title: "Second Post",
+      date: "May 2, 2026",
+      preview: "Another interesting post..."
+    }
+  ]
+};
+
+export default blog;
